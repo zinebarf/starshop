86a01eb6337db60af9e37f4d17ccad9d9a164354
Akram Fouzi <akramfouzi3@gmail.com>
2024-06-22 00:40:47 +0100

    Resolved the ship problem

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -2,6 +2,8 @@
 
 namespace App\Model;
 
+use App\Model\StarshipStatusEnum;
+
 class Starship
 {
     public function __construct(
@@ -9,7 +11,7 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status
     ) {
     }
 
@@ -33,7 +35,7 @@ class Starship
         return $this->captain;
     }
 
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
